c2b6a245fa4003bb9163c211687eba7596bb9b6f
zhernick <71762095+zhernick@users.noreply.github.com>
2023-10-06 00:53:37 +0500

    Update PlayerAttack.cs

--- HealthBar/PlayerAttack.cs
+++ HealthBar/PlayerAttack.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 
 namespace Player
 {
-    public class PlayerAttack : MonoBehaviour, IDamagable
+    public class PlayerAttack : MonoBehaviour
     {
-        private HashAnimations animBase = new HashAnimations();
-        public bool attacking;
-        private bool holdingAttackButton;
-        private Animator _anim;
         [SerializeField] private int playerHealth;
         [SerializeField] private int playerCurrentHealth;
-        [SerializeField] private int playerAttack;
-        private int baseHeath = 100;
-        private int baseAttack = 100;
-
         [SerializeField] private HealthBar healthBar;
         [SerializeField] private bool smoothHealth;
-
-        public static PlayerAttack Instance;
-
-        private void Awake()
-        {
-            Instance = this;
-            _anim = GetComponent<Animator>();
-        }
 
         private void Start()
         {
@@ -34,8 +18,6 @@ namespace Player
 
         void Update()
         {
-            Attack();
-
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 TakeDamage(10);
@@ -51,68 +33,6 @@ namespace Player
             playerCurrentHealth = playerHealth;
             
             healthBar.SetHealth(playerHealth, playerCurrentHealth);
-        }
-
-        public int GetHealth()
-        {
-            return playerHealth;
-        }
-        public int GetAttack()
-        {
-            return playerAttack;
-        }
-
-        private void CheckStats()
-        {
-            if (playerAttack < baseAttack) playerAttack = baseAttack;
-            if (playerHealth < baseHeath) playerHealth = baseHeath;
-        }
-
-        public void AddStats(int health, int attack)
-        {
-            playerHealth += health;
-            playerAttack += attack;
-            CheckStats();
-        }
-        
-        public void ReduceStats(int health, int attack)
-        {
-            playerHealth -= health;
-            playerAttack -= attack;
-            CheckStats();
-        }
-
-        void Attack()
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                holdingAttackButton = true;
-            }else if (Input.GetKeyUp(KeyCode.E))
-            {
-                holdingAttackButton = false;
-            }
-            
-            if (holdingAttackButton)
-            {
-                // Когда атакую
-                attacking = true;
-                _anim.CrossFade(animBase.Attack1, 0);
-            }
-        }
-
-        // не лезть сюда
-        public void JoystickAttack()
-        {
-            holdingAttackButton = true;
-        }
-        public void JoystickAttackStop()
-        {
-            holdingAttackButton = false;
-        }
-        public void ExitingAttack()
-        {
-            _anim.CrossFade(animBase.Idle, 0);
-            attacking = false;
         }
 
         public void TakeDamage(int damage)
